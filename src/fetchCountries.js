de7ffc1bed7d6f17cf0f7db8e16f0fff34bb2904
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-03-21 15:44:00 +0100

    end

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+// const FILTER_RESPONSE = `?fields=name,flags,name,capital,population,languages`;
+
+export function fetchCountries(name) {
+  return fetch(
+    `${BASE_URL}${name}?fields=name,flags,name,capital,population,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+// error name mast be specific - "Too many matches found. Please enter a more specific name.":
+// error 404 - "Oops, there is no country with that name".
+
+const ref = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  inform: document.querySelector('.country-info'),
+};
+console.log(ref.input.textContent === ('Ukraine'));
+ref.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  const trimValue = ref.input.value.trim();
+
+  cleanHtml();
+  if (trimValue !== '') {
+    fetchCountries(trimValue).then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length === 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      } else if (data.length >= 2 && data.length <= 10) {
+        Notiflix.Notify.success('OK');
+        createCountryList(data);
+      } else if (data.length === 1) {
+        createCountry(data);
+      }
+    });
+  }
+}
+
+function createCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li class="country">
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+         <p>${country.name.official}</p>
+                </li>`;
+    })
+    .join('');
+  ref.list.innerHTML = markup;
+}
+function createCountry(countries) {
+  console.log(countries);
+  const markup = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      const langList = Object.values(languages).join(', ');
+      return `<li class='country'>
+          <div class='country-item'>
+            <img class='country-image' src="${flags.svg}" />
+            <h1 class='country-name'>${name.official}</h1>
+          </div>
+          <p><b>Capital</b>: ${capital}</p>
+          <p><b>Population</b>: ${population}</p>
+          <p><b>Languages</b>: ${langList}
+        </li>`;
+    })
+    .join('');
+  ref.inform.innerHTML = markup;
+}
+
+function cleanHtml() {
+  ref.list.innerHTML = '';
+  ref.inform.innerHTML = '';
+}
